9442ee0740c002202b71e3bb040e071ba7b0706c
Matt <85322+mattmassicotte@users.noreply.github.com>
2021-11-02 21:31:27 -0400

    Package structure

---
+++ Tests/GramophoneTests/GramophoneTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Gramophone
+
+final class GramophoneTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Gramophone().text, "Hello, World!")
+    }
+}
