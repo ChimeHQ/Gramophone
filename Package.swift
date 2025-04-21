0f7aae31d225428dafb1ac7d3196b23077bb2249
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-04-21 16:10:52 -0400

    Attempging to get things building in CI

--- Package.swift
+++ Package.swift
@@ -1,25 +1,32 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 
 import PackageDescription
 
 let package = Package(
-    name: "Gramophone",
-	platforms: [.macOS(.v10_15), .iOS(.v13)],
-    products: [
-        .library(name: "Gramophone", targets: ["Gramophone"]),
+	name: "Gramophone",
+	platforms: [
+		.macOS(.v10_15),
+		.macCatalyst(.v13),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v6),
+		.visionOS(.v1),
+	],
+	products: [
+		.library(name: "Gramophone", targets: ["Gramophone"]),
 		.executable(name: "gram", targets: ["CLITool"]),
-    ],
-    dependencies: [
+	],
+	dependencies: [
 		.package(url: "https://github.com/ChimeHQ/Flexer", branch: "main"),
 		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-    ],
-    targets: [
-        .target(
-            name: "Gramophone",
-            dependencies: ["Flexer"]),
-        .testTarget(
-            name: "GramophoneTests",
-            dependencies: ["Gramophone"]),
+	],
+	targets: [
+		.target(
+			name: "Gramophone",
+			dependencies: ["Flexer"]),
+		.testTarget(
+			name: "GramophoneTests",
+			dependencies: ["Gramophone"]),
 		.executableTarget(
 			name: "CLITool",
 			dependencies: [
@@ -27,15 +34,5 @@ let package = Package(
 				.product(name: "ArgumentParser", package: "swift-argument-parser"),
 			]
 		),
-    ]
+	]
 )
-
-let swiftSettings: [SwiftSetting] = [
-	.enableExperimentalFeature("StrictConcurrency")
-]
-
-for target in package.targets {
-	var settings = target.swiftSettings ?? []
-	settings.append(contentsOf: swiftSettings)
-	target.swiftSettings = settings
-}
